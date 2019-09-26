5e1e8f78436307cdef03b0478b6a074304b2b8b0
Talha-Jawed <jawedtalha0@gmail.com>
2019-09-26 22:57:29 +0500

    fix

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io").listen(server);
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 io.on("connection", socket => {
   console.log("a user connected :D");
