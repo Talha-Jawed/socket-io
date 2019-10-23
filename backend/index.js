147242df47fbb8df9f7635b7514bc970ac0d0a99
Talha-Jawed <jawedtalha0@gmail.com>
2019-10-23 22:57:35 +0500

    remove console

--- backend/index.js
+++ backend/index.js
@@ -7,21 +7,18 @@ const port = process.env.PORT || 3001;
 io.on("connection", socket => {
   console.log("a user connected :D");
   socket.on("chat", msg => {
-    console.log(msg);
     socket.broadcast.emit("chat", msg);
 
   });
 
   // handle typing
   socket.on("typing", data => {
-    console.log(data);
     socket.broadcast.emit("typing", data);
   });
 
 
   //Handle read message
   socket.on("read", obj => {
-    console.log(obj , 'seen====>');
     socket.broadcast.emit("read", obj);
   });
 
